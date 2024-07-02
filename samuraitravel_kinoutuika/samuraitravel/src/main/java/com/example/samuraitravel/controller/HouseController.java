8f8b3f408ff4ff3f5828a1fc831fe481df30a07b
sato-at <fukurou0art@gmail.com>
2024-07-03 01:36:44 +0900

    レビュー機能追加

--- samuraitravel_kinoutuika/samuraitravel/src/main/java/com/example/samuraitravel/controller/HouseController.java
+++ samuraitravel_kinoutuika/samuraitravel/src/main/java/com/example/samuraitravel/controller/HouseController.java
@@ -1,9 +1,12 @@
 package com.example.samuraitravel.controller;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,51 +15,62 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.samuraitravel.entity.House;
+import com.example.samuraitravel.entity.Review;
+import com.example.samuraitravel.entity.User;
 import com.example.samuraitravel.form.ReservationInputForm;
 import com.example.samuraitravel.repository.HouseRepository;
+import com.example.samuraitravel.repository.ReviewRepository;
+import com.example.samuraitravel.security.UserDetailsImpl;
+import com.example.samuraitravel.service.ReviewService;
 
 @Controller
 @RequestMapping("/houses")
 public class HouseController {
-	private final HouseRepository houseRepository;
-
-	public HouseController(HouseRepository houseRepository) {
-		this.houseRepository = houseRepository;
-	}
-
-	@GetMapping
-	public String index(@RequestParam(name = "keyword", required = false) String keyword,
-			@RequestParam(name = "area", required = false) String area,
-			@RequestParam(name = "price", required = false) Integer price,
-			@RequestParam(name = "order", required = false) String order,
-			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
-			Model model) 
-	{
+    private final HouseRepository houseRepository;     
+    private final ReviewRepository reviewRepository;   
+    private final ReviewService reviewService;  
+ 
+    public HouseController(HouseRepository houseRepository, ReviewRepository reviewRepository, ReviewService reviewService) {
+       this.houseRepository = houseRepository;      
+       this.reviewRepository = reviewRepository;
+       this.reviewService = reviewService;
+    }    
+  
+    @GetMapping
+    public String index(@RequestParam(name = "keyword", required = false) String keyword,
+                        @RequestParam(name = "area", required = false) String area,
+                        @RequestParam(name = "price", required = false) Integer price,  
+                        @RequestParam(name = "order", required = false) String order,
+                        @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+                        Model model) 
+    {
 		Page<House> housePage;
 
 		// orderパラメータの値が"priceAsc"以外（nullまたは"createdAtDesc"）の場合は、新着順で並べ替え
 		// keywordが入っていたら
 		if (keyword != null && !keyword.isEmpty()) {
 			if (order != null && order.equals("priceAsc")) {
-				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%", "%" + keyword + "%", pageable);
+				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%",
+						"%" + keyword + "%", pageable);
 			} else {
-				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByCreatedAtDesc("%" + keyword + "%", "%" + keyword + "%", pageable);
+				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByCreatedAtDesc("%" + keyword + "%",
+						"%" + keyword + "%", pageable);
 			}
-		// areaが入っていたら
+			// areaが入っていたら
 		} else if (area != null && !area.isEmpty()) {
 			if (order != null && order.equals("priceAsc")) {
 				housePage = houseRepository.findByAddressLikeOrderByPriceAsc("%" + area + "%", pageable);
 			} else {
 				housePage = houseRepository.findByAddressLikeOrderByCreatedAtDesc("%" + area + "%", pageable);
 			}
-		// priceが入っていたら
+			// priceが入っていたら
 		} else if (price != null) {
 			if (order != null && order.equals("priceAsc")) {
 				housePage = houseRepository.findByPriceLessThanEqualOrderByPriceAsc(price, pageable);
 			} else {
 				housePage = houseRepository.findByPriceLessThanEqualOrderByCreatedAtDesc(price, pageable);
 			}
-		// orderが入っていたら
+			// orderが入っていたら
 		} else {
 			if (order != null && order.equals("priceAsc")) {
 				housePage = houseRepository.findAllByOrderByPriceAsc(pageable);
@@ -73,14 +87,26 @@ public class HouseController {
 
 		return "houses/index";
 	}
-	
-	@GetMapping("/{id}")
-    public String show(@PathVariable(name = "id") Integer id, Model model) {
-        House house = houseRepository.getReferenceById(id);
+    
+    @GetMapping("/{id}")
+    public String show(@PathVariable(name = "id") Integer id, Model model, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
+        House house = houseRepository.getReferenceById(id);        
+        boolean hasUserAlreadyReviewed = false;        
+         
+        if (userDetailsImpl != null) {
+            User user = userDetailsImpl.getUser();
+            hasUserAlreadyReviewed = reviewService.hasUserAlreadyReviewed(house, user);           
+        }
+         
+        List<Review> newReviews = reviewRepository.findTop6ByHouseOrderByCreatedAtDesc(house);        
+        long totalReviewCount = reviewRepository.countByHouse(house);         
         
-        model.addAttribute("house", house); 
+        model.addAttribute("house", house);  
         model.addAttribute("reservationInputForm", new ReservationInputForm());
+        model.addAttribute("hasUserAlreadyReviewed", hasUserAlreadyReviewed);
+        model.addAttribute("newReviews", newReviews);        
+        model.addAttribute("totalReviewCount", totalReviewCount);        
         
         return "houses/show";
-    }
+    }    
 }
--- samuraitravel_kinoutuika/samuraitravel/src/main/resources/schema.sql
+++ samuraitravel_kinoutuika/samuraitravel/src/main/resources/schema.sql
@@ -46,15 +46,28 @@ CREATE TABLE IF NOT EXISTS verification_tokens (
  );
  
  CREATE TABLE IF NOT EXISTS reservations (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    house_id INT NOT NULL,
+    user_id INT NOT NULL,
+    checkin_date DATE NOT NULL,
+    checkout_date DATE NOT NULL,
+    number_of_people INT NOT NULL,
+    amount INT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (house_id) REFERENCES houses (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+ CREATE TABLE IF NOT EXISTS reviews (
      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      house_id INT NOT NULL,
      user_id INT NOT NULL,
-     checkin_date DATE NOT NULL,
-     checkout_date DATE NOT NULL,
-     number_of_people INT NOT NULL,
-     amount INT NOT NULL,
+     score INT NOT NULL,
+     content TEXT NOT NULL,    
      created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
      updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+     UNIQUE (house_id, user_id),
      FOREIGN KEY (house_id) REFERENCES houses (id),
-     FOREIGN KEY (user_id) REFERENCES users (id)
+     FOREIGN KEY (user_id) REFERENCES users (id) 
  );
